d83fdbf4d4c0131fe8bf450bffc8318320671075
Lorand Jakab <lojakab@cisco.com>
2014-03-04 15:26:05 +0200

    Fix the setting of the noeth flow member
    Triggered by Jesse's review (comment #5)

--- datapath/flow_netlink.c
+++ datapath/flow_netlink.c
@@ -509,10 +509,10 @@ static int ovs_key_from_nlattrs(struct sw_flow_match *match, u64 attrs,
 				eth_key->eth_src, ETH_ALEN, is_mask);
 		SW_FLOW_KEY_MEMCPY(match, eth.dst,
 				eth_key->eth_dst, ETH_ALEN, is_mask);
+		SW_FLOW_KEY_PUT(match, phy.noeth, false, is_mask);
 		attrs &= ~(1ULL << OVS_KEY_ATTR_ETHERNET);
-	} else {
+	} else if (!is_mask)
 		SW_FLOW_KEY_PUT(match, phy.noeth, true, is_mask);
-	}
 
 	if (attrs & (1ULL << OVS_KEY_ATTR_VLAN)) {
 		__be16 tci;
